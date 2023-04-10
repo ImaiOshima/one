911e3d6ee9ac279f325f259c02d7f08b7a2a8d30
DESKTOP-CM9A6O1\61635 <616357796@qq.com>
2023-04-11 00:48:04 +0800

    quchong

--- src/main/java/com/example/one/map/LambdaMapFilter.java
+++ src/main/java/com/example/one/map/LambdaMapFilter.java
@@ -1,5 +1,11 @@
 package com.example.one.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * @Classname LambdaMapFilter
  * @Description TODO
@@ -9,6 +15,23 @@ package com.example.one.map;
  */
 public class LambdaMapFilter {
     public static void main(String[] args) {
-        
+        Map<Integer,String> a = new HashMap<>();
+        Map<Integer,String> b = new HashMap<>();
+        Map<Integer,String> c = new HashMap<>();
+        Map<Integer,String> d = new HashMap<>();
+        a.put(1,"a");
+        b.put(2,"b");
+        c.put(3,"c");
+        d.put(1,"d");
+        List<Map<Integer,String>> list = Arrays.asList(a,b,c,d);
+        filter(list,1);
+    }
+
+    public static List<Map<Integer,String>>  filter(List<Map<Integer,String>> list,Integer key){
+        List<Map<Integer, String>> res = list.stream()
+                .filter(x-> !x.containsKey(key))
+                .collect(Collectors.toList());
+        res.forEach(System.out::println);
+        return res;
     }
 }
